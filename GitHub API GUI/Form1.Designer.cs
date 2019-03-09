feca9aa2390bb33637ca7ccf613078cedff26e6e
Roomsaver <tropicalpikachu2@gmail.com>
2019-03-09 16:26:05 -0500

    Initial Commit

---
+++ GitHub API GUI/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace GitHub_API_GUI
+{
+    partial class GithubGUIForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GithubGUIForm));
+            this.txtTag = new System.Windows.Forms.Label();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // txtTag
+            // 
+            this.txtTag.AutoSize = true;
+            this.txtTag.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTag.Location = new System.Drawing.Point(32, 11);
+            this.txtTag.Name = "txtTag";
+            this.txtTag.Size = new System.Drawing.Size(56, 29);
+            this.txtTag.TabIndex = 1;
+            this.txtTag.Text = "Tag";
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRefresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRefresh.Location = new System.Drawing.Point(23, 53);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 32);
+            this.btnRefresh.TabIndex = 2;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Text = "notifyIcon1";
+            // 
+            // GithubGUIForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(120, 88);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.txtTag);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "GithubGUIForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Atmosphère";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label txtTag;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ GitHub API GUI/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Octokit;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace GitHub_API_GUI
+{
+    public partial class GithubGUIForm : Form
+    {
+        public GithubGUIForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtTag.Text = getReleaseTag();
+            /*try
+            {
+                Stream streamTry = new FileStream("H:\\latestRelease.dat", System.IO.FileMode.Open, FileAccess.Read);
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error);
+                Serializer objNew = new Serializer();
+                objNew.release = "0.0.0";
+                IFormatter formatterNew = new BinaryFormatter();
+                Stream streamNew = new FileStream("H:\\latestRelease.dat", System.IO.FileMode.Create, FileAccess.Write);
+
+                formatterNew.Serialize(streamNew, objNew);
+                streamNew.Close();
+
+            } 
+
+            Stream stream = new FileStream("H:\\latestRelease.dat", System.IO.FileMode.Open, FileAccess.Read);
+            IFormatter formatter = new BinaryFormatter();
+            Serializer obj = (Serializer)formatter.Deserialize(stream);
+            
+            if (!obj.release.Equals(getReleaseTag()))
+            {
+                MessageBox.Show("New Atmosphere release available!");
+            }
+
+        */    
+        }
+
+        //Minimize to Tray
+
+        /*
+        void ImportStatusForm_Resize(object sender, EventArgs e)
+        {
+            notifyIcon.Icon = ((System.Drawing.Icon)(System.Resources.ResourceManager.GetObject("notifyIcon.Icon")));
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                notifyIcon.Visible = true;
+                notifyIcon.ShowBalloonTip(3000);
+                this.ShowInTaskbar = false;
+            }
+        }
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+            notifyIcon.Visible = false;
+        }
+        */
+
+        static string getReleaseTag()
+        {
+            var client = new GitHubClient(new ProductHeaderValue("Insert Client Name Here"));
+            var tokenAuth = new Credentials("Enter Auth Token Here");
+            client.Credentials = tokenAuth;
+            var user = client.User.Get("Atmosphere-NX").Result;
+            var apiInfo = client.GetLastApiInfo();
+            //txtUsername.Text = user.Name;
+            var releases = client.Repository.Release.GetLatest("Atmosphere-NX", "Atmosphere").Result;
+
+            return releases.TagName;
+        }
+       
+        
+        
+        /*
+        static void SaveReleaseTag(Release releases)
+        {
+            Serializer obj = new Serializer();
+            obj.release = releases.TagName;
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream("H:\\latestRelease.dat", System.IO.FileMode.Create, FileAccess.Write);
+
+            formatter.Serialize(stream, obj);
+            stream.Close();
+        }
+
+        public class Serializer
+        {
+            public string release;
+        }
+        */
+    }
+}
